a10fa5a60451f498032ea4f8e12cb3b491b75019
Marco Pragliola <marcopragliola@gmail.com>
2020-02-19 10:06:07 +0100

    Initial commit

---
+++ src/go.mod
@@ -0,0 +1,8 @@
+module github.com/mpragliola/docker-go-example
+
+go 1.13
+
+require (
+	github.com/gorilla/mux v1.7.4
+	github.com/joho/godotenv v1.3.0
+)
---
+++ src/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	initializeService()
+
+	r := mux.NewRouter()
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprintf(w, "Bestia immane")
+	})
+
+	http.Handle("/", r)
+
+	var serverPort = os.Getenv("SERVICE_INTERNAL_PORT")
+
+	if serverPort == "" {
+		serverPort = "80"
+	}
+
+	log.Println(fmt.Sprintf("Starting server, listening [port %s]", serverPort))
+	log.Fatal(http.ListenAndServe(":"+serverPort, nil))
+}
+
+func initializeService() {
+	log.Println(fmt.Sprintf("Service started [PID %d]", os.Getpid()))
+
+	err := godotenv.Load("../.env")
+	if err != nil {
+		log.Print("No .env file, using env variables as they are")
+	}
+}
